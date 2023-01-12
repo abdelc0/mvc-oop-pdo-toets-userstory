b4e1e5f97719382ca8a617345a0d0e409ef8da57
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2023-01-12 12:44:52 +0100

    vierde  commit

--- app/db/mvc-framework.sql
+++ app/db/mvc-framework.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 04 okt 2022 om 06:47
--- Serverversie: 5.7.31
+-- Gegenereerd op: 12 jan 2023 om 12:01
+-- Serverversie: 5.7.36
 -- PHP-versie: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -18,35 +18,109 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mvc-2109a`
+-- Database: `mvc-framework`
 --
-CREATE DATABASE IF NOT EXISTS `mvc-2109a` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mvc-2109a`;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `country`
+-- Tabelstructuur voor tabel `auto`
 --
 
-DROP TABLE IF EXISTS `country`;
-CREATE TABLE IF NOT EXISTS `country` (
-  `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Name` varchar(100) NOT NULL,
-  `CapitalCity` varchar(100) NOT NULL,
-  `Continent` enum('Afrika','Noord-Amerika','Zuid-Amerika','Oceani&euml;','Europa','Azië','Antarctica') NOT NULL,
-  `Population` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `auto`;
+CREATE TABLE IF NOT EXISTS `auto` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Kenteken` varchar(8) NOT NULL,
+  `Type` varchar(100) NOT NULL,
+  `InstructeurId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `InstructeurId` (`InstructeurId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Gegevens worden geëxporteerd voor tabel `country`
+-- Gegevens worden geëxporteerd voor tabel `auto`
 --
 
-INSERT INTO `country` (`Id`, `Name`, `CapitalCity`, `Continent`, `Population`) VALUES
-(1, 'Nederland', 'Amsterdam', 'Europa', 17000000),
-(2, 'Belgi&euml;', 'Brussel', 'Europa', 11500000),
-(4, 'Marokko', 'Rabat', 'Afrika', 36900000);
+INSERT INTO `auto` (`Id`, `Kenteken`, `Type`, `InstructeurId`) VALUES
+(1, 'AU-67-IO', 'Golf', 3),
+(2, 'TH-78-KL', 'Ferrari', 2),
+(3, '90-KL-TR', 'Fiat 500', 4),
+(4, 'YY-OP-78', 'Mercedes', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `instructeur`
+--
+
+DROP TABLE IF EXISTS `instructeur`;
+CREATE TABLE IF NOT EXISTS `instructeur` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Naam` varchar(100) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `instructeur`
+--
+
+INSERT INTO `instructeur` (`Id`, `Naam`, `Email`) VALUES
+(1, 'Groen', 'groen@gmail.com'),
+(2, 'Manhoi', 'manhoi@gmail.com'),
+(3, 'Zoyi', 'zoyi@gmail.com'),
+(4, 'Berthold', 'berthold@gmail.com'),
+(5, 'Denekamp', 'denekamp@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `mankement`
+--
+
+DROP TABLE IF EXISTS `mankement`;
+CREATE TABLE IF NOT EXISTS `mankement` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `AutoId` int(11) NOT NULL,
+  `Datum` date NOT NULL,
+  `Mankement` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Autoid` (`AutoId`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `mankement`
+--
+
+INSERT INTO `mankement` (`Id`, `AutoId`, `Datum`, `Mankement`) VALUES
+(1, 4, '2023-01-04', 'Profiel rechterband minder dan 2mm'),
+(2, 2, '2023-01-02', 'Rechter achterlicht kapot'),
+(3, 1, '2023-01-02', 'Spiegel links afgebroken'),
+(4, 2, '2023-01-06', 'Bumper rechtsachter ingedeukt'),
+(5, 2, '2023-01-08', 'Radio kapot'),
+(8, 2, '2023-01-10', 'test'),
+(9, 2, '2023-01-12', 'sss'),
+(10, 2, '2023-01-12', 'aa'),
+(11, 2, '2023-01-12', 'aa'),
+(12, 2, '2023-01-12', 'aa'),
+(13, 2, '2023-01-12', 'jjj'),
+(14, 2, '2023-01-12', 'aas'),
+(15, 2, '2023-01-12', 'asda'),
+(16, 2, '2023-01-12', 'aa'),
+(17, 2, '2023-01-12', 'aa'),
+(18, 2, '2023-01-12', 'aa'),
+(19, 2, '2023-01-12', 'hoi'),
+(20, 2, '2023-01-12', 'hhs');
+
+
+ALTER TABLE `auto`
+  ADD CONSTRAINT `InstructeurId` FOREIGN KEY (`InstructeurId`) REFERENCES `instructeur` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `mankement`
+--
+ALTER TABLE `mankement`
+  ADD CONSTRAINT `Autoid` FOREIGN KEY (`AutoId`) REFERENCES `auto` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
